bae3237b37bdcaa1b7fabf9e81e551f4feaf4738
Henry Xue <86995623+henry0225@users.noreply.github.com>
2021-12-09 07:21:20 +0000

    Improved UI

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Chess from 'chess.js';
 import clone from 'just-clone';
 
 export default function App({ boardWidth }) {
+  const chessboardRef = useRef();
   const [game, setGame] = useState(new Chess());
   const [currentTimeout, setCurrentTimeout] = useState(undefined);
-  const chessboardRef = useRef();
+  const [rightClickedSquares, setRightClickedSquares] = useState({});
+  const [moveSquares, setMoveSquares] = useState({});
+  const [optionSquares, setOptionSquares] = useState({});
   const [arrows, setArrows] = useState([]);
   var [boardOrientation, setBoardOrientation] = useState('white');
   var [color, setColor] = useState('white')
@@ -154,7 +157,10 @@ export default function App({ boardWidth }) {
 
     // illegal move
     if (move === null) return false;
-
+    setMoveSquares({
+      [sourceSquare]: { backgroundColor: 'rgba(255, 255, 0, 0.4)' },
+      [targetSquare]: { backgroundColor: 'rgba(255, 255, 0, 0.4)' }
+    });
     // store timeout so it can be cleared on undo/reset so computer doesn't execute move
     const newTimeout = setTimeout(makeMove, 500);
     setCurrentTimeout(newTimeout);
@@ -165,6 +171,55 @@ export default function App({ boardWidth }) {
 
   }
 
+  function onMouseOverSquare(square) {
+    getMoveOptions(square);
+  }
+
+  // Only set squares to {} if not already set to {}
+  function onMouseOutSquare() {
+    if (Object.keys(optionSquares).length !== 0) setOptionSquares({});
+  }
+
+  function getMoveOptions(square) {
+    const moves = game.moves({
+      square,
+      verbose: true
+    });
+    if (moves.length === 0) {
+      return;
+    }
+
+    const newSquares = {};
+    moves.map((move) => {
+      newSquares[move.to] = {
+        background:
+          game.get(move.to) && game.get(move.to).color !== game.get(square).color
+            ? 'radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)'
+            : 'radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)',
+        borderRadius: '50%'
+      };
+      return move;
+    });
+    newSquares[square] = {
+      background: 'rgba(255, 255, 0, 0.4)'
+    };
+    setOptionSquares(newSquares);
+  }
+
+  function onSquareClick() {
+    setRightClickedSquares({});
+  }
+
+  function onSquareRightClick(square) {
+    const colour = 'rgba(0, 0, 255, 0.4)';
+    setRightClickedSquares({
+      ...rightClickedSquares,
+      [square]:
+        rightClickedSquares[square] && rightClickedSquares[square].backgroundColor === colour
+          ? undefined
+          : { backgroundColor: colour }
+    });
+  }
   return (
     <div>
       <Chessboard
@@ -175,10 +230,19 @@ export default function App({ boardWidth }) {
         boardOrientation={boardOrientation}
         customArrows={arrows}
         position={game.fen()}
+        onMouseOverSquare={onMouseOverSquare}
+        onMouseOutSquare={onMouseOutSquare}
+        onSquareClick={onSquareClick}
+        onSquareRightClick={onSquareRightClick}
         onPieceDrop={onDrop}
         customBoardStyle={{
           borderRadius: '4px',
           boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5)'
+        }}
+        customSquareStyles={{
+          ...moveSquares,
+          ...optionSquares,
+          ...rightClickedSquares
         }}
         ref={chessboardRef}
       />
